0d948836eb1dfef8991d1d08a9f3bf7c1d7a094b
naresh <naresh223@gmail.com>
2022-07-25 21:20:32 +0530

    25-Jul-2022: Naresh: Added print statement to BrowserTest.java file

--- Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
+++ Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
@@ -3,8 +3,7 @@ package com.selenium.scripts;
 public class BrowserTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello world..!");
 	}
 
 }
