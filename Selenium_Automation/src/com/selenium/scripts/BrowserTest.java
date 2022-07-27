023c8a1fe5faa91aa49aabf1983980269e26c5c9
kishoregoud27 <109577113+kishoregoud27@users.noreply.github.com>
2022-07-27 21:24:48 +0530

    Deleted the print statement

--- Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
+++ Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
@@ -3,7 +3,6 @@ package com.selenium.scripts;
 public class BrowserTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world..!");
 		System.out.println("Welcome to Github..!");
 		System.out.println("Welcome to Gitbash..!");
 	}
