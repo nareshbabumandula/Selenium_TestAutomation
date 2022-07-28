5024c7e03962a90854c96dcacd5aea8582891aab
naresh <naresh223@gmail.com>
2022-07-28 21:13:09 +0530

    28-Jul-2022: Naresh: Automating browser using Selenium

--- Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
+++ Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
@@ -1,11 +1,47 @@
 package com.selenium.scripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class BrowserTest {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Github..!");
-		System.out.println("Welcome to Gitbash..!");
-		System.out.println("Generated SSH Key for jaipal..!");
+	static WebDriver driver;
+	
+	static void launchBrowser(String browser) throws InterruptedException {
+		switch (browser) {
+		case "edge":
+		    System.setProperty("webdriver.edge.driver", "./browsers/msedgedriver.exe");
+			driver = new EdgeDriver();
+		    break;
+		case "chrome":
+		    System.setProperty("webdriver.chrome.driver", "./browsers/chromedriver.exe");
+			driver = new ChromeDriver();
+		    break;
+		default:
+			System.out.println("Invalid browser");
+			break;
+		}
+		driver.get("https://www.mycontactform.com/");
+		driver.manage().window().maximize();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.navigate().to("https://www.mycontactform.com/samples.php");
+		System.out.println(driver.getCurrentUrl());
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		System.out.println(driver.getPageSource());
+		
+		//driver.findElement(By.id("user")).sendKeys("ramesh");
+		//driver.findElement(By.id("pass")).sendKeys("Secure*1234");
+		Thread.sleep(4000);
+		driver.quit();
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		launchBrowser("chrome");
 	}
 
 }
