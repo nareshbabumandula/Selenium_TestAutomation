16d89b95ee468aad246c8250fa7a81cc286c393c
naresh <naresh223@gmail.com>
2022-08-03 20:36:18 +0530

    Locators and Page Object Model

--- Selenium_Automation/src/com/selenium/scripts/LocatorsTest.java
+++ Selenium_Automation/src/com/selenium/scripts/LocatorsTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LocatorsTest {
+import com.object.repository.Homepage;
+
+public class LocatorsTest extends Homepage{
 
 	WebDriver driver;
 	
@@ -14,7 +16,23 @@ public class LocatorsTest {
 		driver.manage().window().maximize();
 		driver.get("https://www.mycontactform.com");
 		driver.findElement(By.id("user")).sendKeys("rakesh");
+		Thread.sleep(2000);
 		driver.findElement(By.name("pass")).sendKeys("Secure*1234");
+		Thread.sleep(2000);
+		driver.findElement(By.className("txt_log")).clear();
+		Thread.sleep(2000);
+		driver.findElement(By.className("txt_log")).sendKeys("Ravi");
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Sample Forms")).click();
+		driver.findElement(sampleForms).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[name='email_to[]']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.tagName("input")).sendKeys("Test Field");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("naresh223@gmail.com");
+		Thread.sleep(2000);	
+		driver.findElement(By.partialLinkText("Feat")).click();
 		Thread.sleep(4000);
 		driver.quit(); // Terminate the browser
 	}
