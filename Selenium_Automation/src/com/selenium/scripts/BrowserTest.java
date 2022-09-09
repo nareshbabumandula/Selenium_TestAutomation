0f6c558d7ff6a0de576df76dee9906923ded5299
kishoregoud27 <109577113+kishoregoud27@users.noreply.github.com>
2022-09-09 10:51:11 +0530

    Update BrowserTest.java

--- Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
+++ Selenium_Automation/src/com/selenium/scripts/BrowserTest.java
@@ -3,9 +3,13 @@ package com.selenium.scripts;
 public class BrowserTest {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Github..!");
+		String name="selenium java";
+                System.out.println("Welcome to Github..!");
 		System.out.println("Welcome to Gitbash..!");
 		System.out.println("Created my branch in git..!");
+		
+		
+		
 	}
 
 }
