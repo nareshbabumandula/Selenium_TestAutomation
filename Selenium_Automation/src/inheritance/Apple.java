566028c84e1de347e23ff60bb6e7ea595a48cc20
jaipal-321 <jaipal.klsm@gmail.com>
2022-09-28 09:18:49 +0530

    Test Commit

---
+++ Selenium_Automation/src/inheritance/Apple.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class Apple extends Fruits {
+	
+String Ttype = "fruit";
+int maxage=20;
+public void Ttype() {
+	System.out.println("Ttype IS : " + Ttype);
+	
+	
+}
+	
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
