260cc8ca1ed9c4730822e6e5f81f345dccb36855
jaipal-321 <jaipal.klsm@gmail.com>
2022-09-29 19:52:13 +0530

    29 sep 2022:jaipal string practice

---
+++ Selenium_Automation/src/com/datatypes/Stringtypes.java
@@ -0,0 +1,28 @@
+package com.datatypes;
+
+public class Stringtypes {
+
+	public static void main(String[] args) {
+		String str="hello world";
+		String str2="HELLO GOOD MORNING";
+		System.out.println(str.toUpperCase());
+		System.out.println(str2.toLowerCase());
+		System.out.println(str.length());
+		System.out.println(str2.toLowerCase());
+		System.out.println(str.equals("HELLO WORLD"));
+		System.out.println(str.equalsIgnoreCase("HELLO WORLD"));
+		System.out.println(str.charAt(0));
+		System.out.println(str2.charAt(12));
+		System.out.println(str2.charAt(9));
+		System.out.println(str.contains("hello"));
+		System.out.println(str + "going to hyderabad");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
