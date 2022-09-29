bf7f89a53e194eab8e024583c2fe2a815821822d
jaipal-321 <jaipal.klsm@gmail.com>
2022-09-29 19:08:56 +0530

    Dataypes push 29 sep 2022

---
+++ Selenium_Automation/src/com/datatypes/Datatypes.java
@@ -0,0 +1,36 @@
+package com.datatypes;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		//premitive data type
+		
+	byte b =103;
+	short s = 2344;
+	long  l =234655;		
+	int a=10;
+	char c='f';
+	double d=234.2567;
+	float f=12.5f;
+	boolean bflag=true;
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(l);
+	System.out.println(a);
+	System.out.println(d);
+	System.out.println(f);
+	System.out.println(bflag);
+	
+	
+	
+		
+		
+	
+		
+	
+		
+			
+
+	}
+
+}
