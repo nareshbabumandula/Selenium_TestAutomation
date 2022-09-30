785ec3219b2a51152da6c62bb0557bbbf9c45cb7
jaipal-321 <jaipal.klsm@gmail.com>
2022-09-30 14:31:46 +0530

    30 sep 2022 Methodoverloading and methodoverridingtest:jaipal

---
+++ Selenium_Automation/src/com/datatypes/MethodOverloadTest.java
@@ -0,0 +1,41 @@
+package com.datatypes;
+
+public class MethodOverloadTest {
+	int a=10;
+	 int b=20;
+	 void addition() {
+	int c=a+b;
+	System.out.println("addition of  a and b : "+c);
+		 
+	 }
+	 void addition(int a ,int b) {
+		 int c=a+b;
+		 System.out.println("addition ofinteger  a and b : "+c);
+	 }
+	 
+		  void addition(int a,int b, int c) {
+			 int d=a+b+c;
+			 System.out.println("addition of  a,b and c is :"+d);
+		  }
+			 
+	 
+
+	public static void main(String[] args) {
+		
+	MethodOverloadTest mt=new MethodOverloadTest();
+	mt.addition();
+	System.out.println(mt.a);
+	System.out.println(mt.b);
+	mt.addition();
+	mt.addition(30, 50);
+	mt.addition(20, 040, 060);
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
---
+++ Selenium_Automation/src/com/datatypes/MethodOverridetest.java
@@ -0,0 +1,25 @@
+package com.datatypes;
+
+public class MethodOverridetest extends MethodOverloadTest {
+	int a=20;
+	 int b=60;
+	 
+
+
+		 
+	 
+	 
+
+	public static void main(String[] args) {
+		MethodOverridetest mt=new MethodOverridetest();
+		mt.addition();
+	System.out.println(mt.a);
+	System.out.println(mt.b);
+	
+		
+	
+	
+
+	}
+
+}
