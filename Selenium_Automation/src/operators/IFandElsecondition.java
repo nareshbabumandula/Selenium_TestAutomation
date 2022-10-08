9e0ef4c387f3858f9021e826a5eb777ab0d19f3d
jaipal-321 <jaipal.klsm@gmail.com>
2022-10-08 21:47:18 +0530

    nested statements 08 oct 2022 jaipal

---
+++ Selenium_Automation/src/operators/IFandElsecondition.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class IFandElsecondition {
+
+	public static void main(String[] args) {
+		int a= 20;
+		int b= 30;
+		if(a>b)
+		{
+		System.out.println("a value is the largest");
+		}
+		else
+		{
+        System.out.println("b value is the largest");
+		}
+	}}
+	
+
+
---
+++ Selenium_Automation/src/operators/Nestedoperator.java
@@ -0,0 +1,48 @@
+package operators;
+
+public class Nestedoperator {
+
+	public static void main(String[] args) {
+		int day =8;
+		if(day ==1)
+		{
+		System.out.println("DISPLAY OF SUNDAY");
+		}
+	
+		else if(day==2)
+		{
+			System.out.println("display of monday");
+		}
+		else if(day==3)
+		{
+			System.out.println("dispaly of tues");
+			
+		}
+		else if(day==4)
+		{
+			System.out.println("dispaly of wedesday");
+			
+		}
+		else if(day==5)
+		{
+			System.out.println("dispaly of thursday");
+			
+		}
+		else if(day==6)
+		{
+			System.out.println("dispaly of friday");
+			
+		}
+			
+		else if(day==7)
+	
+		System.out.println("dispaly of saturday");
+		else
+		{
+			System.out.println("invalid week number");
+		}
+		
+	}
+		
+	
+}
