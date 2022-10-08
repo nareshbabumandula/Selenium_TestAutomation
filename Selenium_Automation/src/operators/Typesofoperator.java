e9d0a6d90cd5ce9482904ab66a69d6bf336a45fb
jaipal-321 <jaipal.klsm@gmail.com>
2022-10-08 18:45:57 +0530

    types of operators operators :08oct 2022 jaipal

---
+++ Selenium_Automation/src/operators/Typesofoperator.java
@@ -0,0 +1,35 @@
+package operators;
+
+public class Typesofoperator {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		System.out.println("arthematic operator");
+		System.out.println(a-b);
+		System.out.println(a+b);
+		System.out.println(a/b);
+		System.out.println(a*b);
+		
+		//RELATIONAL Operator
+		System.out.println(a==b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+	
+		
+	
+a=10;
+a=a-1;
+b=40;
+b=b+1;
+System.out.println(b);
+System.out.println(a);
+
+	}
+}
+
+
+
+
