bad3b7930ce91418fd133ef15ad13eb69a97f365
jaipal-321 <jaipal.klsm@gmail.com>
2022-10-08 22:02:17 +0530

    switchstatement: jaipal

---
+++ Selenium_Automation/src/operators/Switchstatements.java
@@ -0,0 +1,26 @@
+package operators;
+
+public class Switchstatements {
+
+	public static void main(String[] args) {
+		int day=55;
+		switch(day)
+		{
+		
+		case 1:System.out.println("sunday");break;
+		case 2:System.out.println("monday");break;
+		case 3:System.out.println("tuesday");break;
+		case 4:System.out.println("thursday");break;
+		case 5:System.out.println("friday");break;
+		case 6:System.out.println("saturday");break;
+		default:System.out.println("invalid this week");
+		
+		
+		
+		
+		}
+		
+
+	}
+
+	}
