dfdd98900ba72fcb22d5aa3ec2663cabbf976ffe
naresh223 <naresh223@gmail.com>
2022-10-13 09:39:24 +0530

    13-Oct-2022: Naresh: Vector and BrokenLinksTest example

---
+++ Selenium_Automation/src/com/java/collections/VectorExample.java
@@ -0,0 +1,64 @@
+package com.java.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+
+	// Java collection is framework or an architecture with which we can store and manipulate the group of objects
+	public static void main(String[] args) {
+		
+		List<String> list = new Vector<String>(); // Upcasting
+		Vector<String> vec = new Vector<String>();
+		
+		vec.add("padma");
+		vec.add("vikas");
+		vec.add("saran");
+		vec.add("jaipal");
+		vec.add("priyaveena");
+		vec.add("himaja");
+		vec.add("renuka");
+		vec.add("priyanka");
+		vec.add("padma");
+		vec.add(null);
+		
+		System.out.println(vec.get(0));
+		System.out.println(vec.get(1));
+		vec.remove(1);
+		System.out.println(vec.get(1));
+		System.out.println(vec.contains("himaja"));
+		System.out.println(vec.isEmpty());
+		System.out.println(vec.size());
+		System.out.println(vec.remove("jaipal"));
+		
+		for (int i = 0; i < vec.size(); i++) {
+			System.out.println(vec.get(i));
+		}
+				
+		// lamda expression on collection
+		System.out.println("Lamda on collection");
+		vec.forEach((n)->System.out.println(n));
+		
+		ArrayList<String> vec1 = new ArrayList<String>();
+		vec1.add("vamshi");
+		vec1.add("shiva");
+		vec1.add("rajesh");
+		vec1.add("neelima");
+		
+		System.out.println("Adding two collections");
+		vec.addAll(vec1); // Add two collections
+		vec.forEach((value)->System.out.println(value));
+		
+		ArrayList vec2 = new ArrayList();
+		vec2.add("naresh");
+		vec2.add(true);
+		vec2.add(1234);
+		vec2.forEach((value)->System.out.println(value));
+		
+		
+		
+					
+	}
+
+}
---
+++ Selenium_Automation/src/com/selenium/scripts/BrokenLinksTest.java
@@ -0,0 +1,63 @@
+package com.selenium.scripts;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinksTest {
+
+	static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "./browsers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://ultimateqa.com/complicated-page");
+		driver.manage().window().maximize(); // Maximize the browser window
+		
+		List<WebElement> nLinks = driver.findElements(By.tagName("a"));
+		System.out.println("No of links found in Sample Forms page are : "  +nLinks.size());
+		
+		for (int i = 0; i < nLinks.size(); i++) {
+			System.out.println(nLinks.get(i).getText());
+		}
+		HttpURLConnection huc = null;
+		int respcode = 200;
+		String url="";
+		String linkName="";
+		
+		java.util.Iterator<WebElement> iter = nLinks.iterator();
+		
+		while (iter.hasNext()) {
+			url = iter.next().getAttribute("href");
+			linkName = iter.next().getText(); // Retrieve link name
+					
+			if (url==null || url.isEmpty()) {
+				System.out.println("URL is not configured properly for link : " +linkName);
+			} 
+			
+			try {
+				huc  = (HttpURLConnection)(new URL(url).openConnection());
+				huc.setRequestMethod("HEAD");
+				respcode = huc.getResponseCode();
+				
+				if (respcode>=400) {
+					System.out.println(linkName + " : is a broken link");
+				} else {
+					System.out.println(linkName + " : is a valid link");
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+				
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
