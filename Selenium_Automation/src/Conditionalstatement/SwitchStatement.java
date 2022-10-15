004b7f9fd802f5454cb34bb510cf49714ca425cb
jaipal-321 <jaipal.klsm@gmail.com>
2022-10-15 22:17:38 +0530

    Switchstatements:jaipal

---
+++ Selenium_Automation/src/Conditionalstatement/SwitchStatement.java
@@ -0,0 +1,27 @@
+package Conditionalstatement;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+		int i=-1;
+		switch(i) {
+		case 1:
+			System.out.println("this is the first element");
+			System.out.println("this is jaipal reddy");
+			break;
+		case 2:
+			System.out.println("this is the second element");
+		break;
+		case 3:
+			System.out.println("This is the third element");
+			break;
+		default:
+			System.out.println("this is the Default number");
+			break;
+		
+		
+	}
+
+	}
+
+}
