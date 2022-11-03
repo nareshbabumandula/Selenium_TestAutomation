9d660b34ecbb7f9e05180676339fe5a4d2c2b4d0
naresh223 <naresh223@gmail.com>
2022-11-03 09:11:07 +0530

    03-Nov-2022: Naresh: Advanced xPath and Css locators

--- Selenium_Automation/src/com/selenium/scripts/LocatorsAdvancedExample.java
+++ Selenium_Automation/src/com/selenium/scripts/LocatorsAdvancedExample.java
@@ -1,9 +1,11 @@
 package com.selenium.scripts;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import com.object.repository.Homepage;
 
@@ -31,6 +33,16 @@ public class LocatorsAdvancedExample extends Homepage{
 		driver.findElement(By.cssSelector("input[id*='jec']")).sendKeys("Test Subject4");
 		Thread.sleep(2000);
 		driver.findElement(By.cssSelector("input[id*='jec']")).clear();
+		driver.findElement(By.cssSelector("input#user")).sendKeys("john smith");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input#user")).clear();
+		driver.findElement(By.cssSelector("input.txt_log")).sendKeys("sjoerd jager");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input.txt_log")).clear();
+
+		driver.findElement(By.cssSelector("input#user")).sendKeys("john smith");
+		driver.findElement(By.cssSelector("input#user")).clear();
+		
 
 		// Advanced methods in XPath Locator
 		driver.navigate().to("https://www.mycontactform.com");
@@ -65,6 +77,30 @@ public class LocatorsAdvancedExample extends Homepage{
 		System.out.println(emailAddressLabel.isEnabled());
 		System.out.println(emailAddressLabel.getText());
 		System.out.println(driver.findElement(By.xpath("//label[@*='email']")).getText());
+		
+		WebElement username = driver.findElement(By.id("user"));
+		System.out.println(username.getAttribute("name"));
+		System.out.println(username.getAttribute("type"));
+		System.out.println(username.getAttribute("class"));
+		System.out.println(username.getAttribute("id"));
+		System.out.println(username.getAttribute("tabindex"));
+		username.sendKeys("testuser");
+		System.out.println(username.getAttribute("value"));
+		
+		driver.navigate().to("http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+		WebElement tool = driver.findElement(By.xpath("//select[@id='dropdowm-menu-1']/following-sibling::*"));
+		System.out.println(tool.getAttribute("id"));
+		
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;  
+		jsExecutor.executeScript("arguments[0].style.border='2px solid red'", tool);
+		Thread.sleep(2000);
+		Select select = new Select(tool); // We use Select class to work with dropdown list box
+		select.selectByIndex(3);
+		Thread.sleep(2000);
+		select.selectByIndex(2);
+		Thread.sleep(2000);
+		
+		
 		Thread.sleep(4000);
 		driver.quit(); // Terminate the browser
 
